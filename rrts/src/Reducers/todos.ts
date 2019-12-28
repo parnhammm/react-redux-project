0f0ba645afb8126b544f2dee1307c00263d9b071
Andrew Parnham <atparnham@gmail.com>
2019-12-28 17:27:12 +0000

    Hook up deletion

--- rrts/src/Reducers/todos.ts
+++ rrts/src/Reducers/todos.ts
@@ -1,10 +1,11 @@
-import { Todo, FetchTodosAction } from "../Actions";
-import { ActionTypes } from "../Actions/types";
+import { Todo, Action, ActionTypes } from "../Actions";
 
-export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+export const todosReducer = (state: Todo[] = [], action: Action) => {
   switch (action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
+    case ActionTypes.deleteTodo:
+      return state.filter((todo: Todo) => todo.id !== action.payload);
     default:
       return state;
   }
